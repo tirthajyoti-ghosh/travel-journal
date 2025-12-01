4b1f8e416b80e39e9f9dcd286a0c901a33abaf25
Tirthajyoti Ghosh <itirthahere@gmail.com>
2025-12-01 14:48:34 +0800

    Configure path aliases and upgrade tentap-editor
    - Add babel-plugin-module-resolver for path alias support
    - Create babel.config.js with @ alias configuration
    - Create metro.config.js for Expo Metro bundler
    - Upgrade @10play/tentap-editor from 0.7.4 to 1.0.1
      - Fixes new architecture compatibility issues
      - Updated to support React Native 0.81.5
    - Update app.json: fix scheme, disable React Compiler, enable tsconfigPaths

---
+++ app/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.jsx',
+            '.json',
+            '.tsx',
+            '.ts',
+          ],
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+    ],
+  };
+};
